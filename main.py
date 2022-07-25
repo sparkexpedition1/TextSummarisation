5696b09f01f1d87af8c0666812dbb3ef18792fea
sparkexpedition1 <61966148+sparkexpedition1@users.noreply.github.com>
2022-07-25 18:30:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import pydaisi as pyd
 import pandas as pd
 def printSummary(text):
     st.header(text)
+
+st.set_page_config(layout = "wide")
+algorithm = st.sidebar.text_input("Text", "Sample Text")
 printSummary(text)
